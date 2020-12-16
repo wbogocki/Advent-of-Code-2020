db7bfd844664183b5e93fc37329733cd318e2dad
Wojciech Bogócki <wojciechbogocki@fastmail.com>
2020-12-16 10:17:21 +0800

    day 15

---
+++ day 15/part2.py
@@ -0,0 +1,25 @@
+init_numbers = [int(num) for num in open("day 15/input.txt").read().split(",")]
+
+past_turns = dict()
+for index, number in enumerate(init_numbers[:-1]):
+    past_turns[number] = index + 1
+
+last_number = init_numbers[-1]
+
+turn = len(past_turns) + 1
+while turn != 30000000:
+    if last_number not in past_turns:
+        # it is spoken for the first time
+        next_number = 0
+    else:
+        # it has been spoken before
+        next_number = turn - past_turns[last_number]
+
+    past_turns[last_number] = turn
+
+    turn += 1
+    last_number = next_number
+
+    # print(f"turn {turn}: {next_number}")
+
+print(f"30000000th number: {last_number}")
